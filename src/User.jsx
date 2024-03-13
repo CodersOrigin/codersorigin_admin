d255b4a4fce61838a51e80780c7ba0abe262700f
Pratik Gaikwad <pratik2000gaikwad@gmail.com>
2024-03-13 12:41:29 +0530

    Refactor code: Fix formatting and add Avatar component

--- src/User.jsx
+++ src/User.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Header from "./components/Header";
 import Preloader from "../src/image/preloader.gif";
+import Avatar from "./components/Avatar";
 
 const User = () => {
   const [userData, setUserData] = useState([]);
@@ -10,20 +11,22 @@ const User = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const authToken = localStorage.getItem('authToken');
+        const authToken = localStorage.getItem("authToken");
         const headers = {
           Authorization: `Bearer ${authToken}`,
         };
 
-        const response = await axios.get(process.env.REACT_APP_GETALL_USER, { headers });
+        const response = await axios.get(process.env.REACT_APP_GETALL_USER, {
+          headers,
+        });
 
-        console.log(userData)
+        console.log(userData);
         setTimeout(() => {
           setUserData(response.data);
           setLoading(false);
         }, 1000);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         setLoading(false); // Set loading to false even if there's an error
       }
     };
@@ -33,32 +36,37 @@ const User = () => {
 
   return (
     <div>
-              <Header />
+      <Header />
       {loading ? (
-        <div className='preloader'>
-          <img className='preloaderimg' src={Preloader} alt="preloader img" />
+        <div className="preloader">
+          <img className="preloaderimg" src={Preloader} alt="preloader img" />
         </div>
       ) : (
-        <div className='user'>
+        <div className="user">
           <div className="usercon">
-            
-            <table >
+            <table>
               <thead>
-                <tr className='head'>
-                  <th style={{ width: '25%' }}>Name</th>
-                  <th style={{ width: '35%' }}>Email</th>
-                  <th style={{ width: '15%' }}>Contact No</th>
-                  <th style={{ width: '10%' }}>Last Login</th>
-                  <th style={{ width: '15%' }}>Account Created On</th>
+                <tr className="head">
+                  <th style={{ width: "25%" }}>Name</th>
+                  <th style={{ width: "35%" }}>Email</th>
+                  <th style={{ width: "15%" }}>Contact No</th>
+                  <th style={{ width: "10%" }}>Last Login</th>
+                  <th style={{ width: "15%" }}>Account Created On</th>
                 </tr>
               </thead>
               <tbody>
                 {userData.map((user) => (
-                  <tr className='bodyrow' key={user._id}>
+                  <tr className="bodyrow" key={user._id}>
+                    <td>
+                      <Avatar src={user.profilePic} alt="User Avatar" />
+                    </td>
                     <td>{user.name}</td>
                     <td>{user.email}</td>
                     <td>{user.contactNumber}</td>
-                    <td>{user.lastLogin.length > 0 ? user.lastLogin[0] : 'N/A'}</td>
+                    <td>
+                      {user.lastLogin.length > 0 ? user.lastLogin[0] : "N/A"}
+                    </td>
+                    <td>{user.isAdmin}</td>
                     <td>{new Date(Number(user.createdOn)).toLocaleString()}</td>
                   </tr>
                 ))}
